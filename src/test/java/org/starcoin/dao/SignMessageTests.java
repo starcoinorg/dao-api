1b4bb132ab725b41058d996c207040bd0ac66e60
yangjiefeng <yangjiefeng@gmail.com>
2022-06-29 22:31:00 +0800

    update test code

--- src/test/java/org/starcoin/dao/SignMessageTests.java
+++ src/test/java/org/starcoin/dao/SignMessageTests.java
@@ -23,12 +23,14 @@ public class SignMessageTests {
 
     @Test
     public void testVerifySignedMessage() throws SerializationError, DeserializationError {
-        String messageBytes = "0xd7f20befd34b9f1ab8aeae98b82a5a516f7b2264616f4964223a22746573745f64616f5f6964222c2270726f706f73616c4e756d626572223a2231222c226163636f756e7441646472657373223a2230783031222c22766f74696e67506f776572223a313131313131312c2263686f69636553657175656e63654964223a307d002032ed52d319694aebc5b52e00836e2f7c7d2c7c7791270ede450d21dbc90cbfa12032ed52d319694aebc5b52e00836e2f7c7d2c7c7791270ede450d21dbc90cbfa101";
+        String messageBytes = "0x4e241dcb1955fdc4811ac3c4abb90f146f7b2264616f4964223a22746573745f64616f5f6964222c2270726f706f73616c4e756d626572223a2231222c226163636f756e7441646472657373223a2230783031222c22766f74696e67506f776572223a313131313131312c2263686f69636553657175656e63654964223a317d00206f1a656de7c85d5d1e936dc80c8f3a8ba16e014003090bb20c207196b4fbab2440e8053432ead6bc1b1063fa986f4626e627556fd6c918e9e46dcc3414ee2a504f7baff920796c69f460399c3887eecf97da0b944baa21ebd68271351551e78e0f01";
         SignedMessage message = SignedMessage.bcsDeserialize(HexUtils.decode(messageBytes));
         boolean checked = SignatureUtils.signedMessageCheckSignature(message);
         assertTrue(checked);
         checked = SignatureUtils.signedMessageCheckAccount(message, new ChainId((byte) 255), null);
         assertTrue(checked);
+        byte[] msgBytes = HexUtils.toPrimitive(message.message.value.toArray(new Byte[0]));
+        System.out.println(new String(msgBytes, StandardCharsets.UTF_8));
     }
 
     @Test
